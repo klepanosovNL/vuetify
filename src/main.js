ce856965685f8c7c97c6300c90e1bc907bc4bedf
klepanosovNL <klepanosovNL@gmail.com>
2020-06-15 10:34:26 +0300

    Added firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as firebase from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -15,5 +16,18 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB7AZ00bQn3mOzMAB66VYspF5v3XfE8gDU',
+      authDomain: 'fb-ads-klp.firebaseapp.com',
+      databaseURL: 'https://fb-ads-klp.firebaseio.com',
+      projectId: 'fb-ads-klp',
+      storageBucket: 'fb-ads-klp.appspot.com',
+      messagingSenderId: '1051507405564',
+      appId: '1:1051507405564:web:bbe3985d5cd0bf010b9d79',
+      measurementId: 'G-SRF6B1ZL8V'
+    })
+    // firebase.analytics();
+  }
 })
