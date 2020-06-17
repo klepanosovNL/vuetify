10675b9175b608284d02af8492c46b50ff584d58
klepanosovNL <klepanosovNL@gmail.com>
2020-06-17 14:21:48 +0300

    creating and processing order

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,9 +1,17 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12 sm6 offset-sm3>
+      <v-flex xs12 class="text-xs-center pt-5" v-if="loading">
+        <v-progress-circular
+          indeterminate
+          :size="100"
+          :width="4"
+          color="purple"
+        ></v-progress-circular>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 v-else-if="!loading && orders.length !== 0">
         <h1 class="text--secondary mb-3">Orders</h1>
-        <v-list three-line subheader>
+        <v-list two-line subheader>
           <v-list-tile
             avatar
             v-for="order in orders"
@@ -11,23 +19,26 @@
           >
             <v-list-tile-action>
               <v-checkbox
-                color="primary"
+                color="success"
                 :input-value="order.done"
                 @change="markDone(order)"
               ></v-checkbox>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>{{ order.name }}</v-list-tile-title>
-              <v-list-tile-sub-title>{{ order.phone }}</v-list-tile-sub-title>
+              <v-list-tile-title>{{order.name}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
             </v-list-tile-content>
             <v-list-tile-action>
               <v-btn
+                :to="'/ad/' + order.adId"
                 class="primary"
-                :to="'/ad/' + order.adID"
-              >open</v-btn>
+              >Open</v-btn>
             </v-list-tile-action>
           </v-list-tile>
         </v-list>
+      </v-flex>
+      <v-flex xs12 class="text-xs-center" v-else>
+        <h1 class="text--secondary">You have no orders</h1>
       </v-flex>
     </v-layout>
   </v-container>
@@ -35,23 +46,25 @@
 
 <script>
   export default {
-    data () {
-      return {
-        orders: [
-          {
-            id: 'q1',
-            name: 'Nik',
-            phone: '12345678',
-            adID: '1',
-            done: false
-          }
-        ]
+    computed: {
+      loading () {
+        return this.$store.getters.loading
+      },
+      orders () {
+        return this.$store.getters.orders
       }
     },
     methods: {
       markDone (order) {
-        order.done = true
+        this.$store.dispatch('markOrderDone', order.id)
+          .then(() => {
+            order.done = true
+          })
+          .catch(() => {})
       }
+    },
+    created () {
+      this.$store.dispatch('fetchOrders')
     }
   }
 </script>
